051f5ecb6005cea9b8aff84abaef3ad4518f7bd1
Daniel Karlsson <karlssberg@gmail.com>
2023-12-31 17:28:22 +0000

    elevated AndSpecification's only constructor to a primary constructor

--- Karlssberg.Motive/And/AndSpecification.cs
+++ Karlssberg.Motive/And/AndSpecification.cs
@@ -2,18 +2,12 @@
 
 namespace Karlssberg.Motive.And;
 
-internal class AndSpecification<TModel, TMetadata> : SpecificationBase<TModel, TMetadata>
+internal class AndSpecification<TModel, TMetadata>(
+    SpecificationBase<TModel, TMetadata> leftOperand,
+    SpecificationBase<TModel, TMetadata> rightOperand) : SpecificationBase<TModel, TMetadata>
 {
-    internal AndSpecification(
-        SpecificationBase<TModel, TMetadata> leftOperand,
-        SpecificationBase<TModel, TMetadata> rightOperand)
-    {
-        LeftOperand = Throw.IfNull(leftOperand, nameof(leftOperand));
-        RightOperand = Throw.IfNull(rightOperand, nameof(rightOperand));
-    }
-
-    public SpecificationBase<TModel, TMetadata> LeftOperand { get; }
-    public SpecificationBase<TModel, TMetadata> RightOperand { get; }
+    public SpecificationBase<TModel, TMetadata> LeftOperand { get; } = Throw.IfNull(leftOperand, nameof(leftOperand));
+    public SpecificationBase<TModel, TMetadata> RightOperand { get; } = Throw.IfNull(rightOperand, nameof(rightOperand));
 
     public override string Description => $"({LeftOperand}) AND ({RightOperand})";
 
