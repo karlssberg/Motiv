3de12eda75abba31c98c517f74d6aa4047783ddd
Daniel Karlsson <karlssberg@gmail.com>
2024-05-02 14:01:25 +0100

    cleaned up tests

--- Motiv.Tests/HigherOrderExplanationSpecTests.cs
+++ Motiv.Tests/HigherOrderExplanationSpecTests.cs
@@ -11,6 +11,7 @@ public class HigherOrderExplanationSpecTests
     [InlineData(1, 3, 5, 9, "is not a pair of even numbers")]
     public void Should_supplant_metadata_from_a_higher_order_spec(int first, int second, int third, int fourth, string expected)
     {
+        // Arrange
         var underlyingSpec =
             Spec.Build<int>(i => i % 2 == 0)
                 .WhenTrue(i => $"{i} is even")
@@ -25,13 +26,18 @@ public class HigherOrderExplanationSpecTests
                 .Create();
 
         var result = spec.IsSatisfiedBy([first, second, third, fourth]);
+
+        // Act
+        var act = result.Explanation.Assertions;
         
-        result.Explanation.Assertions.Should().BeEquivalentTo(expected);
+        // Assert
+        act.Should().BeEquivalentTo(expected);
     }
     
     [Fact]
     public void Should_preserve_the_description_of_the_underlying_()
     {
+        // Arrange
         var underlyingSpec =
             Spec.Build<int>(i => i % 2 == 0)
                 .WhenTrue("is even")
@@ -45,7 +51,11 @@ public class HigherOrderExplanationSpecTests
                 .WhenFalse("is not a pair of even numbers")
                 .Create();
 
-        spec.Statement.Should().Be("is a pair of even numbers");
+        // Act
+        var act = spec.Statement;
+        
+        // Assert
+        act.Should().Be("is a pair of even numbers");
     }
 
     [Theory]
@@ -59,6 +69,7 @@ public class HigherOrderExplanationSpecTests
     [InlineData(false, false, false, "third all false")]
     public void Should_only_yield_the_most_recent_when_multiple_yields_are_chained(bool first, bool second, bool third, string expected)
     {
+        // Arrange
         var underlying =
             Spec.Build((bool b) => b)
                 .WhenTrue("is true")
@@ -85,8 +96,12 @@ public class HigherOrderExplanationSpecTests
                 .Create();
 
         var result = spec.IsSatisfiedBy([first, second, third]);
+
+        // Act
+        var act = result.Explanation.Assertions;
         
-        result.Explanation.Assertions.Should().BeEquivalentTo(expected);
+        // Assert
+        act.Should().BeEquivalentTo(expected);
     }
     
     [Theory]
@@ -100,6 +115,7 @@ public class HigherOrderExplanationSpecTests
     [InlineData(false, false, false, "is false")]
     public void Should_yield_the_most_deeply_nested_reason_when_requested(bool first, bool second, bool third, string expected)
     {
+        // Arrange
         var underlyingSpec =
             Spec.Build<bool>(b => b)
                 .WhenTrue("is true")
@@ -125,24 +141,28 @@ public class HigherOrderExplanationSpecTests
                 .Create("all even");
 
         var result = spec.IsSatisfiedBy([first, second, third]);
+
+        // Act
+        var act = result.GetRootAssertions();
         
-        result.GetRootAssertions().Should().BeEquivalentTo(expected);
+        // Assert
+        act.Should().BeEquivalentTo(expected);
     }
     
     [Theory]
-    [InlineData(2, 4, 6, 8, true, "all even")]
-    [InlineData(2, 4, 6, 9, false, "not all even: 9 is odd")]
-    [InlineData(1, 4, 6, 9, false, "not all even: 1 and 9 are odd")]
-    [InlineData(1, 3, 6, 9, false, "not all even: 1, 3, and 9 are odd")]
-    [InlineData(1, 3, 5, 9, false, "not all even: 1, 3, 5, and 9 are odd")]
-    public void Should_allow_regular_true_yield_to_be_used_with_a_higher_order_yield_false(
+    [InlineData(2, 4, 6, 8, true)]
+    [InlineData(2, 4, 6, 9, false)]
+    [InlineData(1, 4, 6, 9, false)]
+    [InlineData(1, 3, 6, 9, false)]
+    [InlineData(1, 3, 5, 9, false)]
+    public void Should_satisfy_regular_true_assertion_yield_to_be_used_with_a_higher_order_yield_of_false_assertions(
         int first, 
         int second, 
         int third,
         int fourth,
-        bool expected,
-        string expectedReason)
+        bool expected)
     {
+        // Arrange
         var underlyingSpec =
             Spec.Build((int i) => i % 2 == 0)
                 .WhenTrue(i => $"{i} is even")
@@ -163,11 +183,59 @@ public class HigherOrderExplanationSpecTests
                 })
                 .Create();
 
-        var act = spec.IsSatisfiedBy([first, second, third, fourth]);
-            
-        act.Assertions.Should().BeEquivalentTo(expectedReason);
-        act.Satisfied.Should().Be(expected);
+        var result = spec.IsSatisfiedBy([first, second, third, fourth]);
+
+        // Act
+        var act = result.Satisfied;
+        
+        // Assert
+        act.Should().Be(expected);
     }
+    
+    
+    [Theory]
+    [InlineData(2, 4, 6, 8, "all even")]
+    [InlineData(2, 4, 6, 9, "not all even: 9 is odd")]
+    [InlineData(1, 4, 6, 9, "not all even: 1 and 9 are odd")]
+    [InlineData(1, 3, 6, 9, "not all even: 1, 3, and 9 are odd")]
+    [InlineData(1, 3, 5, 9, "not all even: 1, 3, 5, and 9 are odd")]
+    public void Should_assert_regular_true_assertion_yield_to_be_used_with_a_higher_order_yield_of_false_assertions(
+        int first, 
+        int second, 
+        int third,
+        int fourth,
+        string expectedReason)
+    {
+        // Arrange
+        var underlyingSpec =
+            Spec.Build((int i) => i % 2 == 0)
+                .WhenTrue(i => $"{i} is even")
+                .WhenFalse(i => $"{i} is odd")
+                .Create("is even spec");
+
+        var spec =
+            Spec.Build(underlyingSpec)
+                .AsAllSatisfied()
+                .WhenTrue("all even")
+                .WhenFalse(results =>
+                {
+                    var serializedModels = results.CausalModels.Serialize();
+                    var modelCount = results.CausalModels.Count;
+                    var isOrAre = modelCount == 1 ? "is" : "are";
+                    
+                    return $"not all even: {serializedModels} {isOrAre} odd";
+                })
+                .Create();
+
+        var result = spec.IsSatisfiedBy([first, second, third, fourth]);
+
+        // Act
+        var act = result.Assertions;
+        
+        // Assert
+        act.Should().BeEquivalentTo(expectedReason);
+    }
+    
     
     [Theory]
     [InlineData(true, "true assertion")]
@@ -175,7 +243,8 @@ public class HigherOrderExplanationSpecTests
     public void Should_harvest_propositionStatement_from_assertion(
         bool model,
         string expectedReasonStatement)
-    { 
+    {
+        // Arrange 
         var expectedReason = string.Join(" & ", Enumerable.Repeat(expectedReasonStatement, 2));
         
         var underlying =
@@ -198,9 +267,13 @@ public class HigherOrderExplanationSpecTests
 
         var spec = withFalseAsScalar & withFalseAsParameterCallback;
         
-        var act = spec.IsSatisfiedBy([model]);
+        var result = spec.IsSatisfiedBy([model]);
+
+        // Act
+        var act = result.Reason;
         
-        act.Reason.Should().Be(expectedReason);
+        // Assert
+        act.Should().Be(expectedReason);
     }
     
     [Theory]
@@ -212,6 +285,7 @@ public class HigherOrderExplanationSpecTests
         string expectedImplicitAssertion,
         string expectedReasonStatement)
     {
+        // Arrange
         var expectedReason = string.Join(" & ",
             expectedAssertion,
             expectedAssertion,
@@ -254,10 +328,14 @@ public class HigherOrderExplanationSpecTests
                    withFalseAsCallback &
                    withFalseAsCallbackThatReturnsACollection &
                    withFalseAsTwoParameterCallbackThatReturnsACollectionWithImpliedName;
+
+        var result = spec.IsSatisfiedBy([model]);
         
-        var act = spec.IsSatisfiedBy([model]);
+        // Act
+        var act = result.Reason;
         
-        act.Reason.Should().Be(expectedReason);
+        // Assert
+        act.Should().Be(expectedReason);
     }
     
     [Theory]
@@ -268,6 +346,7 @@ public class HigherOrderExplanationSpecTests
         string expectedAssertion,
         string expectedReasonStatement)
     {
+        // Arrange
         var expectedReason = string.Join(" & ",
             expectedAssertion,
             expectedAssertion,
@@ -310,10 +389,14 @@ public class HigherOrderExplanationSpecTests
                    withFalseAsParameterCallback &
                    withFalseAsTwoParameterCallback &
                    withFalseAsTwoParameterCallbackThatReturnsACollection;
+
+        var result = spec.IsSatisfiedBy([model]);
         
-        var act = spec.IsSatisfiedBy([model]);
+        // Act
+        var act = result.Reason;
         
-        act.Reason.Should().Be(expectedReason);
+        // Assert
+        act.Should().Be(expectedReason);
     }
     
     [Theory]
@@ -323,7 +406,8 @@ public class HigherOrderExplanationSpecTests
         bool model,
         string expectedAssertion,
         string expectedReasonStatement)
-    { 
+    {
+        // Arrange 
         var expectedReason = string.Join(" & ", 
             expectedAssertion,
             expectedAssertion,
@@ -366,10 +450,14 @@ public class HigherOrderExplanationSpecTests
                    withFalseAsParameterCallback &
                    withFalseAsTwoParameterCallback &
                    withFalseAsTwoParameterCallbackThatReturnsACollection;
+
+        var result = spec.IsSatisfiedBy([model]);
         
-        var act = spec.IsSatisfiedBy([model]);
+        // Act
+        var act = result.Reason;
         
-        act.Reason.Should().Be(expectedReason);
+        // Assert
+        act.Should().Be(expectedReason);
     }
     
     [Theory]
@@ -378,7 +466,8 @@ public class HigherOrderExplanationSpecTests
     public void Should_use_the_propositional_statement_in_the_reason_when_true_assertion_uses_a_two_parameter_callback_that_returns_a_collection(
         bool model,
         string expectedReasonStatement)
-    { 
+    {
+        // Arrange 
         var expectedReason = string.Join(" & ", Enumerable.Repeat(expectedReasonStatement, 3));
         
         var underlying =
@@ -410,9 +499,13 @@ public class HigherOrderExplanationSpecTests
         var spec = withFalseAsScalar &
                    withFalseAsCallback &
                    withFalseAsCallbackThatReturnsACollection;
+
+        var result = spec.IsSatisfiedBy([model]);
         
-        var act = spec.IsSatisfiedBy([model]);
+        // Act
+        var act = result.Reason;
         
-        act.Reason.Should().Be(expectedReason);
+        // Assert
+        act.Should().Be(expectedReason);
     }
 }
