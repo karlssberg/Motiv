9ab7ec5740b772f503e5c12aa4ba4e9635497a8b
Daniel Karlsson <karlssberg@gmail.com>
2025-09-07 11:31:58 +0100

    Add Motiv.CodeFix and Motiv.CodeFix.Tests projects; update System.Collections.Immutable package version

---
+++ src/Motiv.CodeFix.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace Motiv.CodeFix.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ src/Motiv.CodeFix/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Motiv.CodeFix
+{
+    public class Class1
+    {
+    }
+}
