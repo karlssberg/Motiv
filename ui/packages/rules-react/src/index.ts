5e8d7ddc63fe3741d0ef2f4f4739eb8ccf467430
Daniel Karlsson <karlssberg@gmail.com>
2026-07-18 02:13:13 +0100

    feat(rules-react): public barrel export

--- ui/packages/rules-react/src/index.ts
+++ ui/packages/rules-react/src/index.ts
@@ -1,1 +1,7 @@
-export {};
+export { RuleEditorProvider, useRuleEditorStore } from './context.js';
+export { useRuleEditor } from './useRuleEditor.js';
+export { useRuleNode, type RuleNodeView } from './useRuleNode.js';
+export { useCatalog, type CatalogState } from './useCatalog.js';
+export { useEvaluation, type EvaluationState } from './useEvaluation.js';
+export { RuleTree, type RuleTreeItem } from './RuleTree.js';
+export { JustificationTree, type JustificationRow } from './JustificationTree.js';
